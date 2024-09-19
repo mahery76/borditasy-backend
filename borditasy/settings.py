ff22839c51da0a236eb525afacf9b2d9f53cf868
Hasinjaka <andrianomehasinjaka@gmail.com>
2024-09-19 22:08:26 +0300

    Update settings.py

--- borditasy/settings.py
+++ borditasy/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-_%@p^v-3t7wtjsxrlo1*i5!-t&knbn2sm*0bbd+gela&66r0sw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Allow all hosts (not recommended for production)
+ALLOWED_HOSTS = ['192.168.88.2','127.0.0.1']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'borditasyapp',
 ]
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'borditasy.urls'
@@ -70,6 +73,39 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'borditasy.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://192.168.88.154:3000",
+    
+    # Add other origins as needed
+]
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOW_CREDENTIALS = True
+
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 
 # Database
